d7ed2670d0ab4075877abdca52e089fb8222f6aa
testtube123 <32375907+testtube123@users.noreply.github.com>
2018-09-17 14:32:10 -0400

    Add files via upload

---
+++ printme.pas
@@ -0,0 +1,21 @@
+program printme;
+var 
+age: integer;
+name: string;
+begin
+
+WriteLn ('How old are you?');
+ReadLn (age);
+WriteLn('What is your name');
+ReadLn (name);
+if age >70 then
+begin
+   
+WriteLn ('You are a Wise Old Grey beard ',name ,('!')); 
+   end
+else if age <70 then
+begin
+WriteLn ('you are pratically a kid' ,name,('!'));
+end;
+end .
+
